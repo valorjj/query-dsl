0cebc495bf7bf52010b476d446b091aebca34fdb
double J <30681841+valorjj@users.noreply.github.com>
2023-05-12 11:01:52 +0900

    init

---
+++ src/test/java/jj/study/querydsl/InflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package jj.study.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
